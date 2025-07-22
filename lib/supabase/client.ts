55fb7a78cc8d7380ebcfefbc968f51a10b32fbd6
AbuAli85 <chairman@falconeyegroup.net>
2025-07-22 21:24:34 +0400

    changes

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -7,3 +7,8 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
 }
+
+export const supabase = createBrowserClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
