e81d6efd13382722f8dbdb5d4a26109f043c7817
fahad alamri <luxsess2001@hotmail.com>
2025-07-24 17:58:33 +0400

    added

---
+++ app/[locale]/crm/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PromoterCRM from "@/components/promoter-crm";
+
+export default function CRMPage() {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-3xl font-bold mb-6">Promoter CRM</h1>
+      <PromoterCRM />
+    </div>
+  );
+} 
